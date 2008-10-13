d9fae18956c9af768ea1611ddc31d02f88bb5493
Arina Neshlyaeva <arina@arina.site>
2008-10-13 23:17:08 +0700

    Added quicksort with specs

---
+++ quick_spec.rb
@@ -0,0 +1,27 @@
+def quick(a)
+  return a if a.min == a.max
+  m = a[rand(a.size)]
+  quick( a.select { |i| i <= m } ) + quick( a.select { |i| i > m } )
+end
+ 
+describe "Quicksort" do
+  it "should sort an empty array" do
+    quick([]).should == []
+  end
+ 
+  it "should sort an array with size 1" do
+    quick([1]).should == [1]
+  end
+  
+  it "should sort already sorted array" do
+    quick([1, 2, 3]).should == [1, 2, 3]
+  end
+  
+  it "should sort unsorted array" do
+    quick([3, 2, 1]).should == [1, 2, 3]
+  end
+ 
+  it "should sort an array with same numbers" do
+    quick([1, 1, 1]).should == [1, 1, 1]
+  end
+end
